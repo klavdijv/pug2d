452fc0e52155cbb2a173f159000b0fb46e90c055
Klavdij Voncina <voncina.klavdij@siol.net>
2011-07-28 12:14:01 +0200

    Added draw module.

---
+++ examples/draw.py
@@ -0,0 +1,59 @@
+'''
+Created on 24. apr. 2011
+
+@author: klavdij
+'''
+
+from pug2d import core, draw
+import sf
+
+class Actor1(core.Actor):
+    def __init__(self, im, x, y):
+        super(Actor1, self).__init__(None)
+        self.object = sf.Sprite(im)
+        self.object.origin = (im.width//2, im.height//2)
+        self.object.position = (x, y)
+    
+    def update(self, game, dt):
+        self.object.rotate(30.0*dt)
+
+
+class Canvas(draw.Canvas):
+    def __init__(self):
+        super(Canvas, self).__init__()
+        self.add_circle(name='c1', x=100, y=100,
+                        radius=50, color=sf.Color.GREEN, z=10)
+        self.add_line(name='l1', p1x=10.0, p1y=10.0,
+                      p2x=210.0, p2y=210.0, thickness=2.0)
+        self.add_text(name='txt1', x=400.0, y=45.0, string='Test', size=36)
+        self.t = 0.0
+    
+    def update(self, parent, game, dt):
+        self.t += dt
+        txt = 'Test'+str(int(0.1*self.t))
+        self['txt1'].string = txt
+        self['c1'].x += 0.1
+        if 5.0 < self.t < 15.0:
+            self.set_visible('l1', False)
+        else:
+            self.set_visible('l1', True)
+        
+        
+class Level1(core.Level):
+
+    def __init__(self):
+        super(Level1, self).__init__()
+        canvas = Canvas()
+        layer = core.Layer()
+        layer.add_plugin(canvas)
+        self.add_layer(layer)
+        self.im0 = sf.Image.load_from_file('princess.png')
+        for x in xrange(50, 600, 50):
+            for y in xrange(50, 400, 80):
+                act = Actor1(self.im0, x, y)
+                layer.add_actor(act)
+
+
+game = core.Game(800, 600)
+level = Level1()
+game.run(level)
---
+++ pug2d/draw.py
@@ -0,0 +1,102 @@
+'''
+Created on 28. jul. 2011
+
+@author: klavdij
+'''
+
+import sf
+
+class CanvasItem(object):
+    def __init__(self, canvas, shape, z=0):
+        self.canvas = canvas
+        self.shape = shape
+        self.visible = True
+        self.z = z
+    
+    def __getattr__(self, name):
+        return getattr(self.shape, name)
+    
+    def __setattr__(self, name, value):
+        if hasattr(self, 'shape') and hasattr(self.shape, name):
+            setattr(self.shape, name, value)
+        else:
+            super(CanvasItem, self).__setattr__(name, value)
+
+
+class Canvas(object):
+    def __init__(self):
+        self.items = {}
+        self._counter = 0
+    
+    def __getitem__(self, key):
+        return self.items[key]
+    
+    def __delitem__(self, key):
+        del self.items[key]
+    
+    def __contains__(self, key):
+        return key in self.items
+    
+    def clear(self):
+        self.items.clear()
+   
+    def add_shape(self, shape, name=None, z=0):
+        self._counter += 1
+        if not name:
+            name = '%item_'+str(self._counter)
+        item = CanvasItem(self, shape, z=z)
+        self.items[name] = item
+    
+    def add_rectangle(self, name=None, left=0, top=0, width=100, height=100,
+                      color=sf.Color.BLACK, outline=0.0, outline_color=None,
+                      z=0):
+        shape = sf.Shape.rectangle(left, top, width, height, color,
+                                   outline, outline_color)
+        self.add_shape(shape, name=name, z=z)
+    
+    def add_line(self, name=None, p1x=0.0, p1y=0.0, p2x=100.0, p2y=100.0,
+                 thickness=1.0, color=sf.Color.BLACK, outline=0.0,
+                 outline_color=None, z=0):
+        shape = sf.Shape.line(p1x, p1y, p2x, p2y, thickness, color,
+                              outline, outline_color)
+        self.add_shape(shape, name=name, z=z)
+    
+    def add_circle(self, name=None, x=0.0, y=0.0, radius=100.0,
+                   color=sf.Color.BLACK, outline=0.0, outline_color=None, z=0):
+        shape = sf.Shape.circle(x, y, radius, color, outline, outline_color)
+        self.add_shape(shape, name=name, z=z)
+    
+    def add_polygon(self, name=None, points=None, color=sf.Color.BLACK,
+                    outline=0.0, outline_color=None, z=0):
+        shape = sf.Shape()
+        if points:
+            for (x, y) in points:
+                shape.add_point(x, y, color, outline_color)
+        if outline:
+            shape.outline_enabled = True
+            shape.outline_thickness = outline
+        else:
+            shape.outline_enabled = False
+        self.add_shape(shape, name=name, z=z)
+    
+    def add_text(self, name=None, x=0.0, y=0.0, string='',
+                 font=sf.Font.DEFAULT_FONT, size=12,
+                 color=sf.Color.BLACK, z=0):
+        shape = sf.Text(string, font, size)
+        shape.position = (x, y)
+        shape.color = color
+        self.add_shape(shape, name=name, z=z)
+    
+    def set_visible(self, key, visible=True):
+        self.items[key].visible = visible
+    
+    def update(self, parent, game, dt):
+        pass
+    
+    def draw(self, parent, window):
+        items = self.items.values()
+        items.sort(key=lambda obj: obj.z)
+        self._sorted = True
+        for item in items:
+            if item.visible:
+                window.draw(item.shape)
